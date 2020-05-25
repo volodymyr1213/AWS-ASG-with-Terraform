0418cd8c988533058e607ffd38635a5d16a88085
Volodymyr Khomenko <45836705+volodymyr1213@users.noreply.github.com>
2020-05-25 12:11:24 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "region" {}
+variable "lc_name" {}
+variable  "image_id" {}
+variable  "instance_type" {}
+variable "sec_group_name" {}
+
+variable  "device_name" {}
+variable "device_type" {}
+variable "volume_size" {}
+variable    "asg_name"  {}
+variable "vpc_zone_identifier1" {}
+variable "vpc_zone_identifier2" {}
+
+variable "vpc_zone_identifier3" {}
+
+variable "min_size" {}
+variable "max_size" {}
+variable "desired_capacity" {}
+variable "wait_for_capacity_timeout" {}
+variable "s3_bucket" {}
+variable "key_name" {}
+variable "tag_value1" {}
+variable "tag_value2" {}
